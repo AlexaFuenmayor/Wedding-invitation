7d2c9764c5c3675c3b9959a34aab87184d5f4ca7
AlexaFuenmayor <estherf.m18@gmail.com>
2025-06-19 18:03:37 -0500

    Permitir escribir texto mas extenso en el input

--- wedding-invitation/src/components/InvitationForm.jsx
+++ wedding-invitation/src/components/InvitationForm.jsx
@@ -231,7 +231,7 @@ function InvitationForm() {
             <textarea
               name="mensaje"
               rows="3"
-              maxLength="300"
+              maxLength="1000"
               value={form.mensaje}
               onChange={handleChange}
               placeholder="¡Escribe Aquí!"
